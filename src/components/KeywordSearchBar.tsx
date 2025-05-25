b1422291f6a25ad4d29323243250a31ca6fb5c6b
bumjin_gwak <bjgwak@kaist.ac.kr>
2025-05-25 15:56:27 +0900

    feat: SelectPage에 검색 기능 추가
    - 검색바에 키워드 검색, 클릭시 토글 기능 추가
    - 없을 시 키워드 자체를 추가

--- src/components/KeywordSearchBar.tsx
+++ src/components/KeywordSearchBar.tsx
@@ -1,10 +1,70 @@
-import { TextField } from "@mui/material";
+import { Box, Button, styled, TextField, Typography } from "@mui/material";
 import { observer } from "mobx-react";
+import { useMemo, useState } from "react";
+import { useStore } from "../store/StoreContext";
+
+const KeywordText = styled(Typography)`
+  &:hover {
+    background-color: gray;
+  }
+`;
 
 const KeywordSearchBar = observer(() => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const store = useStore();
+  if (!store) return;
+
+  const searchedKeywords = useMemo(() => {
+    return [...store.keywords].filter((val) => val.startsWith(searchTerm));
+  }, [searchTerm, store.keywords]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleAdd = () => {
+    store.addKeywords(searchTerm);
+    setSearchTerm("");
+  };
+
+  const handleToggle = (keyword: string) => {
+    store.toggleKeywords(keyword);
+    setSearchTerm("");
+  };
+
   return (
     <>
-      <TextField></TextField>
+      <TextField value={searchTerm} onChange={handleSearchChange}></TextField>
+      {searchTerm && searchedKeywords.length > 0 && (
+        <Box
+          sx={{
+            marginTop: 1,
+            border: "1px solid #ddd",
+            borderRadius: "4px",
+            padding: 1,
+          }}
+        >
+          {searchedKeywords.map((val) => (
+            <KeywordText
+              key={val}
+              sx={{ padding: "4px 8px" }}
+              onClick={() => {
+                handleToggle(val);
+              }}
+            >
+              {val}
+            </KeywordText>
+          ))}
+        </Box>
+      )}
+      {searchTerm && searchedKeywords.length === 0 && (
+        <Box sx={{ marginTop: 1, padding: 1 }}>
+          <Typography color="textSecondary">
+            일치하는 키워드가 없습니다. 추가할까요?
+          </Typography>
+          <Button onClick={handleAdd}>추가</Button>
+        </Box>
+      )}
     </>
   );
 });
